7954ac403d85abe058888319b31d7e11c21b6514
Dimetris-s <73003261+Dimetris-s@users.noreply.github.com>
2021-09-09 14:23:49 +0300

    filter hw

--- src/components/groupList.jsx
+++ src/components/groupList.jsx
@@ -8,7 +8,6 @@ const GroupList = ({
     selectedItem,
     onItemChange
 }) => {
-    if (typeof items === "object") {
         return (
             <ul className="list-group">
                 {Object.keys(items).map(key => (
@@ -25,26 +24,6 @@ const GroupList = ({
                 ))}
             </ul>
         );
-    }
-
-    if (Array.isArray(items)) {
-        return (
-            <ul className="list-group">
-                {items.map(item => (
-                    <li
-                        key={item[valueProp]}
-                        className={`list-group-item ${
-                            selectedItem === item ? "active" : ""
-                        }`}
-                        onClick={() => onItemChange(item)}
-                        role="button"
-                    >
-                        {item[contentProp]}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
 };
 GroupList.defaultProps = {
     valueProp: "_id",
